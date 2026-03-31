71b1b9ffe0c2780d5516bdfc2b0e74eed21e2c4b
InterlaserRH <isabela.alves@interlaser.com.br>
2026-03-31 07:43:40 -0300

    V2

--- db.py
+++ db.py
@@ -1,15 +1,9 @@
 import os
 import psycopg
 
-
 def get_connection():
-    return psycopg.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        port=int(os.getenv("DB_PORT", "5432")),
-        dbname=os.getenv("DB_NAME", "Interlaser"),
-        user=os.getenv("DB_USER", "postgres"),
-        password=os.getenv("DB_PASSWORD", "admin123"),
-    )
+    url = os.getenv("DATABASE_URL")
+    return psycopg.connect(url)
 
 
 # Compatibilidade com versões antigas do app.py
